53f5f6b66d6ce85975c90900024151fec1995821
voccionetto <voccio@gmail.com>
2026-07-21 02:07:12 -0300

    ignore folders added

--- recipes/clean_studio.py
+++ recipes/clean_studio.py
@@ -1,4 +1,4 @@
-"""Neutral bass-oriented clean foundation."""
+"""Studio DI - Ultra Clean"""
 
 from gt100_builder import PatchBuilder
 from gt100_builder.enums import CompType, PreampType
@@ -8,10 +8,32 @@ def build():
     return (
         PatchBuilder("NATURAL CLEAN")
         .name("Clean Studio")
-        .preamp(type=PreampType.NATURAL_CLEAN, gain=38, bass=55, middle=48, treble=58, level=70)
-        .compressor(type=CompType.BASS_COMP, sustain=30, attack=40, level=55)
-        .eq(low_gain=3, mid_gain=2, high_gain=1)
-        .disable("delay")
+
+        .preamp(
+            type=PreampType.NATURAL_CLEAN,
+            gain=28,
+            bass=52,
+            middle=50,
+            treble=54,
+            level=78,
+        )
+
+        .compressor(
+            type=CompType.BASS_COMP,
+            sustain=22,
+            attack=18,
+            level=60,
+        )
+
+        .eq(
+            low_gain=2,
+            mid_gain=0,
+            high_gain=1,
+        )
+
         .disable("chorus")
+        .disable("delay")
+        .disable("reverb")
+
         .build()
     )
